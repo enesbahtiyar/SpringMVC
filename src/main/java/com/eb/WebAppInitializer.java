493ccca27650fa9507d383b4134cc44253b8859f
Enes Bahtiyar <97892255+enesbahtiyar@users.noreply.github.com>
2023-07-14 18:52:29 +0300

    SpringMVC configurations

---
+++ src/main/java/com/eb/WebAppInitializer.java
@@ -0,0 +1,35 @@
+package com.eb;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//instead of web.xml we had earlier we are going to use this class for configuration
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer
+{
+
+    //getting the configuration related to the hibernate and database
+    @Override
+    protected Class<?>[] getRootConfigClasses()
+    {
+        return new Class[0];
+    }
+
+    //MVC configurations
+    @Override
+    protected Class<?>[] getServletConfigClasses()
+    {
+        return new Class[]
+                {
+                        WebMvcConfig.class
+                };
+    }
+
+    //Which url would go to which servlet
+    @Override
+    protected String[] getServletMappings()
+    {
+        return new String[]
+        {
+                "/"
+        };
+    }
+}
---
+++ src/main/java/com/eb/WebMvcConfig.java
@@ -0,0 +1,42 @@
+package com.eb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan //("com.eb")
+public class WebMvcConfig implements WebMvcConfigurer
+{
+    @Bean
+    public InternalResourceViewResolver resolver()
+    {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+
+        //setting the view class to .jsp
+        resolver.setViewClass(JstlView.class);
+        //specify the folder to check for views
+        resolver.setPrefix("/WEB-INF/views/");
+        //specify the extension
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    //specifying the resources that'll be used for my front-end
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler();
+
+        //find the resources from the path
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources")
+                .setCachePeriod(0);  //giving a period time for caching
+    }
+}
